ed83b2d865834e46a6849e4814c7ae1b07d22224
serainaschoeb <seraina.schoeb@unibas.ch>
2023-05-10 15:57:40 +0200

    Now there is a bash script that upon running adds a function to the ~/.bashrc file, that lets us execute our functionality from everywhere.

--- src/setup.c
+++ src/setup.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
 
-// when filesystem is set up, should alter ~/.bashrc and add path of tfs
+// TODO: when filesystem is set up, should alter ~/.bashrc and add path of tfs
 
 FILE *fptr;
 
 int main()
 {
-    fptr = fopen("~/.bashrc", "a"); //a for append
-    fputs("nhfs(){}", fptr); // still causes segfault, why?
-    fclose(fptr);
+    int status1 = system("chmod u+x bash_function.sh");
+    int status2 = system("./bash_function.sh");
     return 1;
 }
